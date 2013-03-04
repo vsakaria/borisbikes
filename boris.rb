cd9f6054a033a23e509f32c44b6eb73d9f9944ed
Vishal Sakaria <vsakaria@gmail.com>
2013-03-04 20:13:35 +0000

    People created and station bike array created

--- boris.rb
+++ boris.rb
@@ -6,9 +6,11 @@
 class Bike
   def  initialize (bike_id, functioning)
     @bike_id, @functioning = bike_id, functioning
-    
   end
 
+  def to_s
+    puts "The bike id is:#{@bike_id} The status of the bike is: #{@functioning}"
+  end
 end
 
 ##############################################################
@@ -25,9 +27,13 @@ class Station
   end
 
   def to_s
-    @bikes.inspect
+    @bikes.to_s
   end
 
+  def release_bike
+    @bikes.pop
+  end
+  
   def report
 
   end
@@ -58,6 +64,11 @@ class Person
   def initialize(id , status)
     @id, @status = id, status 
   end
+
+  def to_s
+    puts "The persons id is:#{@id} The status of the person is: #{@status}"
+  end
+
 end
 
 
@@ -66,13 +77,29 @@ end
 class Control
 
   def initialize
-      station = Station.new
+     createStation
+     createPeople
+  end
+
+  def createStation
+     station = Station.new
       
       10.times do |i| 
-      station << Bike.new(i, true)   
+        station << Bike.new(i + 1, true)   
       end 
       puts station.to_s
+      puts station.release_bike
+      end
+
+  def createPeople
+     @people = []
+     #How do I refactor this to make it init an array and assign in one line.
+
+    10.times do |i| 
+      @people << Person.new(i + 1, rand(2).zero?)
+    end
     
+    puts @people.to_s
   end
 
 end
